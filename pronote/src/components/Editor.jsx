af2da389979f2b78cdde4402c7d025676c811469
Vimal M <vimalnavee2110@gmail.com>
2021-10-12 21:59:55 +0530

    Progression 2

--- pronote/src/components/Editor.jsx
+++ pronote/src/components/Editor.jsx
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import "./Editor.css"
 
 export default class Editor extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            value: '',
+        }
+    }
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="input">
                     <h3>Input</h3>
-                    <textarea className="input-text" />
+                    <textarea onChange={this.handleChange} defaultValue={this.state.value} className="input-text" />
                 </div>
                 <div className="pronote">
                     <h3>Pro Note</h3>
-                    <div className="output-text"></div>
+                    <div className="output-text">{this.state.value}</div>
                 </div>
 
             </div>
